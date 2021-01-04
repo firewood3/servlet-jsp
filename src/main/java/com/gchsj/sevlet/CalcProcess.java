ab78ace788e8b4dd01993a7691f41b7d78759550
SEOKJIN_HONG <gchsj9838@gmail.com>
2021-01-05 05:12:14 +0900

    서블릿으로 작동하는 동적 계산기 추가

---
+++ src/main/java/com/gchsj/sevlet/CalcProcess.java
@@ -0,0 +1,53 @@
+package com.gchsj.sevlet;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/calc-process")
+public class CalcProcess extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String value = request.getParameter("v");
+        String op = request.getParameter("op");
+        String dot = request.getParameter("dot");
+
+        String exp = "";
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for(Cookie cookie : cookies) {
+                if (cookie.getName().equals("exp")) {
+                    exp = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if(op!=null && op.equals("=")) {
+            ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+            try {
+                exp = String.valueOf(engine.eval(exp));
+            } catch (ScriptException e) {
+                e.printStackTrace();
+            }
+        } else {
+            exp += (value == null)? "":value;
+            exp += (op == null)? "":op;
+            exp += (dot == null)? "":dot;
+        }
+
+        Cookie cookie = new Cookie("exp", exp);
+        response.addCookie(cookie);
+
+        response.sendRedirect("calc-page");
+    }
+}
